d3ad290f3b8589f3b91a59549cc139b59a0cec08
ww <sucaiz@q.com>
2019-03-18 18:38:05 +0800

    add admin themplate and admin login route

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yunxi
+ * Date: 2019/3/18 0018
+ * Time: 17:53
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Base\BaseController;
+
+class LoginController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handleLogin(){
+
+    }
+}
